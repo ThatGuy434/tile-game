19c57294ee14fa8cd3a93a7ed1d9b690dcc905d7
ThatGuy434 <92959469+ThatGuy434@users.noreply.github.com>
2021-11-09 20:07:15 +1300

    Add files via upload

--- tilemap.py
+++ tilemap.py
@@ -1,8 +1,10 @@
-import pygame as py
+import pygame as pg
+import pytmx
 from settings import *
 
 def collide_hit_rect(one, two):
     return one.hit_rect.colliderect(two.rect)
+
 
 class Map:
     def __init__(self, filename):
@@ -16,14 +18,38 @@ class Map:
         self.width = self.tilewidth * TILESIZE
         self.height = self.tileheight * TILESIZE
 
+class TiledMap:
+    def __init__(self, filename):
+        tm = pytmx.load_pygame(filename, pixelalpha=True)
+        self.width = tm.width * tm.tilewidth
+        self.height = tm.height * tm.tileheight
+        self.tmxdata = tm
+
+    def render(self, surface):
+        ti = self.tmxdata.get_tile_image_by_gid
+        for layer in self.tmxdata.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x, y, gid, in layer:
+                    tile = ti(gid)
+                    if tile:
+                        surface.blit(tile, (x * self.tmxdata.tilewidth, y * self.tmxdata.tileheight))
+    def make_map(self):
+        temp_surface = pg.Surface((self.width, self.height))
+        self.render(temp_surface)
+        return temp_surface
+
+
 class Camera:
     def __init__(self, width, height):
-        self.camera = py.Rect(0, 0, width, height)
+        self.camera = pg.Rect(0, 0, width, height)
         self.width = width
         self.height = height
 
     def apply(self, entity):
         return entity.rect.move(self.camera.topleft)
+
+    def apply_rect(self, rect):
+        return rect.move(self.camera.topleft)
 
     def update(self, target):
         x = -target.rect.centerx + int(WIDTH / 2)
@@ -34,4 +60,4 @@ class Camera:
         y = min(0, y) # top limit
         x = max(-(self.width - WIDTH), x) # right side limit
         y = max(-(self.height - HEIGHT), y) # bottom limit
-        self.camera = py.Rect(x, y, self.width, self.height)
+        self.camera = pg.Rect(x, y, self.width, self.height)
